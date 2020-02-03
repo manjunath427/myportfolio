3887847b15501d2282ef546e4fa84a467c68154f
manjunath <manjunath427>
2020-02-03 13:07:34 +0530

    my portfolio

---
+++ build/precache-manifest.b1caed1f808f13df7521979793fb13e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "808d0a551396fd672e54",
+    "url": "/static/css/main.f5024af9.chunk.css"
+  },
+  {
+    "revision": "808d0a551396fd672e54",
+    "url": "/static/js/main.808d0a55.chunk.js"
+  },
+  {
+    "revision": "4d497ceebc387a1b2f4c",
+    "url": "/static/js/1.4d497cee.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "71a682ef7a1a1ad46b607dddb0223a9e",
+    "url": "/static/media/nandan.71a682ef.jpg"
+  },
+  {
+    "revision": "63190af71adeb5d5df6ca22c5d09938f",
+    "url": "/index.html"
+  }
+];
---
+++ src/resumeData.js
@@ -0,0 +1,135 @@
+let resumeData = {
+  "imagebaseurl": "https://rbhatia46.github.io/",
+  "name": "MANJUNATH BAVI",
+  "role": "Full Stack Developer",
+  // "linkedinId":"Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I like  in various parts of frontend development and backend development like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/manju-b-335470190/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/manjunath427",
+      "className": "fa fa-github"
+    },
+    {
+      
+    }
+  ],
+  "aboutme": "I am currently doing a Full Stack Developer Course at TarzanSkills. I am a self taught Full Stack Web Developer, currently diving deeper into Django framework and Reactjs. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  "address": "Bengaluru    India",
+  "phoneNumber": "9148511831",
+  "email": "bhavimanjunath1995@gmail.com",
+  "resume": [
+    {
+      "name": "Resume",
+      "url": "https://drive.google.com/file/d/1EqMTf7A4rtl4_e0v1nBgV9TMRbgu0UkD/view?ths=true",
+      "className": "fa fa-file fa-5x"
+    }],
+
+
+  "education": [
+    {
+      "UniversityName": "Dr Ambedkar of Institute Technology Bengaluru",
+      "specialization": "Computer Science Engg",
+      "MonthOfPassing": "Aug",
+      "YearOfPassing": "2019",
+
+    },
+    {
+      "UniversityName": "B.V.V.S Polytechnic Bagalkot",
+      "specialization": "Information Science Engg",
+      "MonthOfPassing": "Jun",
+      "YearOfPassing": "2016",
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Real Time Skills Private Limited",
+      "specialization": "Full Stack Engineer in Training",
+      "MonthOfLeaving": "10/2019 - Current",
+
+
+
+
+    },
+  ],
+  "skillsDescription": "",
+  "skills": [
+    {
+      "skillname": "Python"
+    },
+    {
+      "skillname": "DjangoFramework"
+    },
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Bootstrap"
+    },
+    {
+      "skillname": "MYSQL"
+    },
+    {
+      "skillname": "PostgreSQL"
+    },
+
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "redux"
+    },
+    {
+      "skillname": "github"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "To-Do",
+      "imgurl": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTq1-1xUN3Kh_XSd-L3jrH2PB0yj3bddTYO7r7Ey5dc-AD7XJxf",
+      "livedemo": "live demo "
+    },
+
+    {
+      "name": "project2",
+      "description": "Sudoku",
+      "imgurl": "https://www.bestgames.com/thumb/Suduku.jpg",
+      "url": "https://nandansudoku.netlify.com/",
+      "livedemo": "live demo "
+    },
+    {
+      "name": "project3",
+      "description": "Cab allocation system",
+      "imgurl": "https://images.unsplash.com/photo-1500289483460-00bdb46204f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=753&q=80"
+      , "livedemo": "live demo "
+    },
+    {
+      "name": "project4",
+      "description": "catalog management system",
+      "imgurl": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQElRxr32yQAm71qwTpjv6OJKz8VVRELQ6j2b33ywFOAJudMrYv"
+      ,"livedemo":"live demo "
+    }
+  ],
+  "testimonials": [
+    {
+      "description": " I am a full stack developer",
+      "name": "Nandan N B"
+    },
+    {
+      "description": "i am currently working on tarzan",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
